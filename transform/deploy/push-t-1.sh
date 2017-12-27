c45efd81a3156d1a5a3d6aa5ec9fdf90c0c9713b
mumenrider <brian.aquinde@outlook.com>
2017-12-27 18:20:25 +0000

    cleaned git cache
    flushed recently ignored gitignore folders

--- transform/deploy/push-t-1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash    
-rsync -avL --progress -e "ssh -i /home/ubuntu/workspace/deploy/PLK.pem" \
-       /home/ubuntu/workspace/platforms/browser/www/* \
-       ubuntu@ec2-52-15-185-191.us-east-2.compute.amazonaws.com:/var/www/html/o
--- transform/deploy/push-t-2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash    
-rsync -avL --progress -e "ssh -i /home/ubuntu/workspace/deploy/PLK-1.pem" \
-       /home/ubuntu/workspace/platforms/browser/www/* \
-       ubuntu@ec2-18-221-198-187.us-east-2.compute.amazonaws.com:/var/www/html/o
